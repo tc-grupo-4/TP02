8d0c619c731fb6fa68e5334da1f54e84128c78cc
Ian Diaz <idiaz@itba.edu.ar>
2019-08-19 17:33:02 -0300

    Agregue codigo en python

---
+++ Ex1/iA/test.py
@@ -0,0 +1,21 @@
+from sympy import Symbol
+from sympy import Function, simplify
+from sympy import *
+from sympy import init_printing
+from sympy.core.numbers import pi, I, oo
+from sympy import re, im
+init_printing()
+a0 = Symbol('a0',real = True)
+r1 = Symbol('R1',real = True)
+r2 = Symbol('R2',real = True)
+r3 = Symbol('R3',real = True)
+r4 = Symbol('R4',real = True)
+w = Symbol('w',real = True)
+f = Symbol('f',real = True)
+
+h = ((a0*r3*r2)/(r1*r2+2*r3*r1-r3*r2)) / (1+ (I*2*pi*f)/w*((r1*r2+2*r3*r1-r3*r2)/(r1*r2+r3*r1-r3*r2)))
+h = simplify(h)
+
+mod = sqrt(re(h)**2 + im(h)**2)
+mod = simplify(mod)
+print(latex(mod))
